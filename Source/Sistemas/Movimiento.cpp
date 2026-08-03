f765e130a531fe5c3f8a3e10910ce0c56bae6969
ferjava <fgsj.fg@gmail.com>
2026-08-03 18:44:39 +0200

    Limpiando entidades que sobrapasan el limite de pantalla

--- Source/Sistemas/Movimiento.cpp
+++ Source/Sistemas/Movimiento.cpp
@@ -20,15 +20,27 @@ void Sistema::Movimiento(entt::registry &reg, float dt) {
     }
     if (reg.all_of<GC::Enemy>(enti)) {
       if (pos.X <= 0) {
+        // TODO: Mueve hacia la derecha
         auto &sp = reg.get<GC::Sprite>(enti);
         sp.source.x = Game::Enemy::MOVE_RIGHT;
         vel.Vx = Game::Enemy::VELOCIDAD_X;
 
       } else if (pos.X + Game::Enemy::TEXTURE_W >= NaviGame::ctx.pantalla.w) {
 
+        // TODO: Mueve hacia la izquierda
         auto &sp = reg.get<GC::Sprite>(enti);
         sp.source.x = Game::Enemy::MOVE_LEFT;
         vel.Vx = -Game::Enemy::VELOCIDAD_X;
+      }
+      // Borrar lso que superan el limite inferior
+      else if (pos.Y + Game::Enemy::TEXTURE_H >= NaviGame::ctx.pantalla.h) {
+        EntidadParaDestruir.push_back(enti);
+      }
+    }
+    // Borramos las balas que se salen de la pantalla
+    if (reg.all_of<GC::BalaEnemy>(enti)) {
+      if (pos.Y + Game::Bala::TAMANO_BALA >= NaviGame::ctx.pantalla.h) {
+        EntidadParaDestruir.push_back(enti);
       }
     }
   }
